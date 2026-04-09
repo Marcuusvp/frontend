cf3b7fd9a5082d8cf30a65ad9dec5a59bd0e20ab
Marcuusvp <marcusviniciuspereira1@gmail.com>
2026-04-09 19:54:32 -0300

    Preparando para o deploy na vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { lazy, Suspense } from 'react'
 import { AuthProvider } from './contexts/AuthProvider'
 import { PrivateRoute } from './components/PrivateRoute'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { Layout } from './components/Layout'
-import { Login } from './pages/Login'
-import { Signup } from './pages/Signup'
-import { ForgotPassword } from './pages/ForgotPassword'
-import { Dashboard } from './pages/Dashboard'
-import { Cards } from './pages/Cards'
-import { Invoice } from './pages/Invoice'
-import { Subscriptions } from './pages/Subscriptions'
-import { Balance } from './pages/Balance'
-import { Profile } from './pages/Profile'
+import { LoadingScreen } from './components/LoadingScreen'
+
+const Login = lazy(() => import('./pages/Login'))
+const Signup = lazy(() => import('./pages/Signup'))
+const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Cards = lazy(() => import('./pages/Cards'))
+const Invoice = lazy(() => import('./pages/Invoice'))
+const Subscriptions = lazy(() => import('./pages/Subscriptions'))
+const Balance = lazy(() => import('./pages/Balance'))
+const Profile = lazy(() => import('./pages/Profile'))
+
+function SuspenseWrapper({ children }) {
+  return <Suspense fallback={<LoadingScreen />}>{children}</Suspense>
+}
 
 function ProtectedLayout({ children }) {
   return (
     <PrivateRoute>
       <Layout>
-        {children}
+        <SuspenseWrapper>{children}</SuspenseWrapper>
       </Layout>
     </PrivateRoute>
   )
@@ -31,9 +38,9 @@ function App() {
         <AuthProvider>
           <Toaster position="top-right" richColors />
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/login" element={<SuspenseWrapper><Login /></SuspenseWrapper>} />
+            <Route path="/signup" element={<SuspenseWrapper><Signup /></SuspenseWrapper>} />
+            <Route path="/forgot-password" element={<SuspenseWrapper><ForgotPassword /></SuspenseWrapper>} />
             <Route
               path="/dashboard"
               element={
